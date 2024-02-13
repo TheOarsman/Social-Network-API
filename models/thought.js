9e0876ed8877db1cc6a8000469d57e3b58f81a44
TheOarsman <heinzulrichv@gmail.com>
2024-02-13 13:50:35 -0500

    setting files up with connections and more

---
+++ models/thought.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const { Schema } = mongoose;
+
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Reaction",
+    },
+  ],
+});
+
+// Create a virtual property `reactionCount` that retrieves the length of the thought's reactions array
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = mongoose.model("Thought", thoughtSchema);
+
+module.exports = Thought;
