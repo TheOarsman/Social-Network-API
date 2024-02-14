e423cbb74f57370d187db0e2878c7615f3688d36
TheOarsman <heinzulrichv@gmail.com>
2024-02-14 13:27:55 -0500

    filling in utils/data utils/seeds

---
+++ utils/seeds.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const { User, Thought } = require("../models");
+const { users, thoughts } = require("./data");
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/social-network-api",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
+
+const seedDatabase = async () => {
+  try {
+    await User.deleteMany({});
+    await Thought.deleteMany({});
+
+    const createdUsers = await User.insertMany(users);
+
+    // Update thoughts to use created user IDs
+    const thoughtsWithUserIds = thoughts.map((thought) => ({
+      ...thought,
+      username: createdUsers.find((user) => user.username === thought.username)
+        ._id,
+    }));
+
+    await Thought.insertMany(thoughtsWithUserIds);
+
+    console.log("Database seeded successfully");
+  } catch (err) {
+    console.error("Error seeding database:", err);
+  } finally {
+    // Close the connection after seeding
+    mongoose.disconnect();
+  }
+};
+
+seedDatabase();
