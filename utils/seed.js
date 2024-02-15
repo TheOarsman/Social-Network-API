ed3a56aa2109dbc5c0789f9ffe147ce7f5267f5d
TheOarsman <heinzulrichv@gmail.com>
2024-02-14 23:30:29 -0500

    working on reactions

--- utils/seed.js
+++ utils/seed.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const { User, Thought } = require("../models");
+const { User, Thought, Reaction } = require("../models");
 const { users, thoughts } = require("./data");
 
 mongoose.connect(
@@ -14,17 +14,36 @@ const seedDatabase = async () => {
   try {
     await User.deleteMany({});
     await Thought.deleteMany({});
+    await Reaction.deleteMany({});
 
     const createdUsers = await User.insertMany(users);
 
-    // Update thoughts to use created user IDs
-    const thoughtsWithUserIds = thoughts.map((thought) => ({
-      ...thought,
-      username: createdUsers.find((user) => user.username === thought.username)
-        ._id,
-    }));
+    // Create thoughts and associate them with the respective users
+    const thoughtsWithUserIds = thoughts.map((thought) => {
+      const user = createdUsers.find(
+        (user) => user.username === thought.username
+      );
+      thought.username = user._id;
+      return thought;
+    });
 
-    await Thought.insertMany(thoughtsWithUserIds);
+    const createdThoughts = await Thought.insertMany(thoughtsWithUserIds);
+
+    // Create reactions and associate them with random users and thoughts
+    for (const thought of createdThoughts) {
+      const randomUser =
+        createdUsers[Math.floor(Math.random() * createdUsers.length)];
+
+      // Create a random number of reactions for each thought
+      const numReactions = Math.floor(Math.random() * 5); // Random number between 0 and 4
+      for (let i = 0; i < numReactions; i++) {
+        await Reaction.create({
+          reactionBody: "Some reaction",
+          username: randomUser._id,
+          thoughtId: thought._id,
+        });
+      }
+    }
 
     console.log("Database seeded successfully");
   } catch (err) {
