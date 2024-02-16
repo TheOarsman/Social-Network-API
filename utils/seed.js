75d73a2646ec11b379bfdb52aa1f69ac9feac9fd
TheOarsman <heinzulrichv@gmail.com>
2024-02-16 17:18:03 -0500

    spelling in comments

--- utils/seed.js
+++ utils/seed.js
@@ -5,9 +5,9 @@ const { users, thoughts, reaction } = require("./data");
 connection.on("error", (err) => err);
 
 const seedDatabase = async () => {
-  console.log("seeding databse");
+  console.log("seeding database");
   try {
-    console.log("deleting users & thoughts");
+    console.log("deleting users & thoughts, and reactions");
     const res = await User.deleteMany({});
     console.log("User Deleted", res);
     const res2 = await Thought.deleteMany({});
