96aacc3fbc6786324c13f52df3ccdc612b779f7e
TheOarsman <heinzulrichv@gmail.com>
2024-02-16 16:22:38 -0500

    removed excess users and thoughts from data.js. Added reaction parameters

--- utils/data.js
+++ utils/data.js
@@ -1,96 +1,39 @@
 const users = [
   {
-    username: "codeNinja42",
-    email: "codeninja42@example.com",
-  },
-  {
     username: "byteMeNow",
     email: "bytemenow@example.com",
-  },
-  {
-    username: "loopLover",
-    email: "looplover@example.com",
-  },
-  {
-    username: "functionMaster",
-    email: "functionmaster@example.com",
   },
   {
     username: "bitWiseGal",
     email: "bitwisegal@example.com",
   },
   {
-    username: "consoleKing",
-    email: "consoleking@example.com",
-  },
-  {
     username: "syntaxSorcerer",
     email: "syntaxsorcerer@example.com",
-  },
-  {
-    username: "codeCrusader",
-    email: "codecrusader@example.com",
-  },
-  {
-    username: "gitGuru",
-    email: "gitguru@example.com",
-  },
-  {
-    username: "binaryBard",
-    email: "binarybard@example.com",
   },
 ];
 
 const thoughts = [
   {
     thoughtText:
-      "Why was the JavaScript developer sad? Because he didn’t know how to `null` his feelings.",
-    username: "codeNinja42",
-  },
-  {
-    thoughtText:
       "Why do programmers prefer dark mode? Because light attracts bugs!",
     username: "byteMeNow",
-  },
-  {
-    thoughtText:
-      'What did the JavaScript function say to the other? "You complete me."',
-    username: "loopLover",
-  },
-  {
-    thoughtText: "Why don’t programmers like nature? It has too many bugs!",
-    username: "functionMaster",
   },
   {
     thoughtText: "Why was the computer cold? It left its Windows open!",
     username: "bitWiseGal",
   },
   {
-    thoughtText:
-      "Why did the developer go broke? Because he used up all his cache!",
-    username: "consoleKing",
-  },
-  {
     thoughtText: "Why do Java developers wear glasses? Because they can’t C#!",
     username: "syntaxSorcerer",
   },
-  {
-    thoughtText:
-      "Why was the code arrested? Because it was caught running in a loop!",
-    username: "codeCrusader",
-  },
-  {
-    thoughtText:
-      "Why did the developer go broke? Because he used up all his cache!",
-    username: "gitGuru",
-  },
-  {
-    thoughtText:
-      "Why do programmers always mix up Halloween and Christmas? Because Oct 31 equals Dec 25!",
-    username: "binaryBard",
-  },
 ];
 
-const reaction = [];
+const reaction = [
+  {
+    reactionText: "",
+    username: "",
+  },
+];
 
 module.exports = { users, thoughts, reaction };
